6b8a3425b0fe3fdf1ac3346c56bc651700e5d312
edmand88 <edmand88@hotmail.com>
2026-01-08 22:00:11 -0500

    updated

--- backend/main.py
+++ backend/main.py
@@ -34,8 +34,6 @@ class User(SQLModel, table=True):
     email: str = Field(unique=True)
     hashed_password: str
     full_name: str | None = None
-    disabled: bool = Field(default=False)
-    points: int = Field(default=0)
 
 class UserCreate(BaseModel):
     username: str
@@ -48,7 +46,6 @@ class UserPublic(BaseModel):
     username: str
     email: str
     full_name: str | None = None
-    points: int
 
 class Token(BaseModel):
     access_token: str
@@ -103,8 +100,6 @@ async def get_current_user(session: SessionDep, token: Annotated[str, Depends(oa
     return user
 
 async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 app = FastAPI()
